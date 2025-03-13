ba12a3e0ccd65149191aa139696dfcfbd35b092f
waldrumpus <waldrumpus@gmail.com>
2025-03-13 20:27:19 +0100

    add Particle

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,29 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use raylib::prelude::*;
+
+pub struct Particle {
+    pub position: Vector2,
+    pub velocity: Vector2,
+    pub acceleration: Vector2,
+}
+
+impl Particle {
+    pub fn new(position: Vector2, velocity: Vector2, acceleration: Vector2) -> Self {
+        Particle {
+            position,
+            velocity,
+            acceleration,
+        }
+    }
+
+    pub fn update(&mut self) {
+        self.velocity += self.acceleration;
+        self.position += self.velocity;
+        self.acceleration *= 0.0;
+    }
+
+    pub fn apply_force(&mut self, force: Vector2) {
+        self.acceleration += force;
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +31,31 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn new_particle() {
+        let velocity = Vector2::new(1.0, 2.0);
+        let acceleration = Vector2::new(3.0, 4.0);
+        let position = Vector2::new(5.0, 6.0);
+        let result = Particle::new(position, velocity, acceleration);
+        assert_eq!(result.velocity, velocity);
+        assert_eq!(result.acceleration, acceleration);
+        assert_eq!(result.position, position);
+    }
+
+    #[test]
+    fn apply_force() {
+        let mut particle = Particle::new(Vector2::new(0.0, 0.0), Vector2::new(0.0, 0.0), Vector2::new(0.0, 0.0));
+        let force = Vector2::new(1.0, 2.0);
+        particle.apply_force(force);
+        assert_eq!(particle.acceleration, force);
+    }
+
+    #[test]
+    fn update() {
+        let mut particle = Particle::new(Vector2::new(0.0, 0.0), Vector2::new(0.0, 0.0), Vector2::new(0.0, 0.0));
+        particle.apply_force(Vector2::new(1.0, 2.0));
+        particle.update();
+        assert_eq!(particle.velocity, Vector2::new(1.0, 2.0));
+        assert_eq!(particle.position, Vector2::new(1.0, 2.0));
+        assert_eq!(particle.acceleration, Vector2::new(0.0, 0.0));
     }
 }
